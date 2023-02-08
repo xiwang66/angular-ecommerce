e26ada94b2ea4d20ef404a9adb78f7fadeffe2dc
Xi Wang <xiwang6688@gmail.com>
2023-02-08 16:01:37 -0500

    switch from http tp https

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oa87jdxq86SbzwOT5d7',
     issuer: 'https://dev-46720326.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
